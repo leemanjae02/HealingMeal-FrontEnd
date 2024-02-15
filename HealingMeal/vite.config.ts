cdb6e3ca2af3a8fc1f89c17586d9b78c45df8c13
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-02-15 21:57:19 +0900

    vite.config.ts 파일 수정

--- HealingMeal/vite.config.ts
+++ HealingMeal/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "",
   esbuild: {
     jsxFactory: "React.createElement", // React 코드에서 사용하는 JSX 문법을 설정
   },
