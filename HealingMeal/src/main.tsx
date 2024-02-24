a95a139d19997b7f521bdddb088bf3253c8c9daa
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2024-02-25 02:51:43 +0900

    라우터 변경

--- HealingMeal/src/main.tsx
+++ HealingMeal/src/main.tsx
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
